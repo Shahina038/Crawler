f0001506e7f3490182efa7f74c71605c9d7dd588
Shahina <shahina@hamon.in>
2022-03-18 16:09:22 +0530

    images download

--- image_crawler.py
+++ image_crawler.py
@@ -1,4 +1,5 @@
 import requests
+import wget
 from bs4 import BeautifulSoup
 header = {
     'User-Agent': 'Mozilla/5.0 (MacintoshIntel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
@@ -16,15 +17,21 @@ def get_amirkhan(site_url):
         ret.append((i['href']))
     return ret
 
+def image_download(imgu_url):
+    r=requests.get(url=imgu_url,headers=header)
+    soup=BeautifulSoup(r.content, features="html.parser")
+    image_div=soup.find("img",{"class":"AssetCard-module__image___dams4"})
+    x= image_div['src']
+    print(x, "\n")
+    return x    
+
    
 
 def main():
     amir = get_amirkhan("https://www.gettyimages.in/photos/aamir-khan-actor")
     head = "https://www.gettyimages.in"
-    for i in amir:
-        print(head+i,
-              "\n")
-
+    for link in amir:
+       wget.download(image_download(head+link))
 
 if __name__ == "__main__":
     main()
